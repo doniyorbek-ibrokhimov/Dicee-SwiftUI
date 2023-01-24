675e5cb309dfdbdde73f54d685c18c6d8779398f
210046 <ac2952602@akfauniversity.org>
2023-01-24 21:00:03 +0500

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by DONIYORBEK IBROKHIMOV on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
